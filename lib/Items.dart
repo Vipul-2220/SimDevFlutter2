ff9f7389d053ae5fdc3b56588e858884c8b3a06f
Vipul Kumar <kumarvipul479@gmail.com>
2021-07-14 22:20:52 +0530

    Updated Item Details

--- lib/Items.dart
+++ lib/Items.dart
@@ -9,7 +9,10 @@ class Items extends StatefulWidget {
 }
 
 class _ItemsState extends State<Items> {
-  Widget itemShow(String itemName, String price, String url) {
+
+  bool like = false;
+
+  Widget itemShow(String itemName, int price, int originalPrice, String url) {
     return Column(
       children: [
         GestureDetector(
@@ -17,7 +20,8 @@ class _ItemsState extends State<Items> {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => showDetails(context, itemName, price, url),
+                builder: (context) =>
+                    showDetails(itemName,price,url),
               ),
             );
           },
@@ -36,17 +40,46 @@ class _ItemsState extends State<Items> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              '$itemName\n'
-              'Rs. $price',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
+            Column(
+              children: [
+                Text(
+                  '$itemName',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Rs. $price',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 5.0,),
+                    Text(
+                      '$originalPrice',
+                      style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.lineThrough,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  like = true;
+                });
+              },
               icon: Icon(
                 Icons.favorite_border,
                 size: 30,
@@ -66,18 +99,20 @@ class _ItemsState extends State<Items> {
     return Row(
       children: [
         Column(children: [
-          itemShow('Men\'s Jacket', '4699', 'images/items/pumaHoodie1.jpg'),
-          itemShow('Louis Phillipe\n Men\'s Shirt', '1899',
+          itemShow('Men\'s Jacket', 4699, 5699, 'images/items/pumaHoodie1.jpg'),
+          itemShow('Louis Phillipe\n Men\'s Shirt', 1899, 2499,
               'images/items/menshirt.jpg'),
-          itemShow('Van Heusen\n Men\'s Chinos', '1999',
+          itemShow('Van Heusen\n Men\'s Chinos', 1999, 3599,
               'images/items/menchinos.jpg'),
         ]),
         Column(
           children: [
+            itemShow('Women\'s Jacket', 7699, 11999,
+                'images/items/womenjacket1.jpg'),
             itemShow(
-                'Women\'s Jacket', '7699', 'images/items/womenjacket1.jpg'),
-            itemShow('Women\'s Shirt', '1499', 'images/items/womenshirt.jpg'),
-            itemShow('Women\'s Jeans', '2699', 'images/items/womenjeans.jpg'),
+                'Women\'s Shirt', 1499, 2899, 'images/items/womenshirt.jpg'),
+            itemShow(
+                'Women\'s Jeans', 2699, 3599, 'images/items/womenjeans.jpg'),
           ],
         ),
       ],
